591974c757faf5e5915653b5f37aba1d0d2b7d96
zjns <1638183271zjn@gmail.com>
2022-01-27 00:03:51 +0800

    Add auto generate chs subtitle support
    添加根据繁中字幕自动翻译生成简中字幕特性，
    翻译能力由繁化姬提供

--- app/src/main/java/me/iacn/biliroaming/XposedInit.kt
+++ app/src/main/java/me/iacn/biliroaming/XposedInit.kt
@@ -102,6 +102,7 @@ class XposedInit : IXposedHookLoadPackage, IXposedHookZygoteInit {
                     startHook(CopyHook(lpparam.classLoader))
                     startHook(LiveRoomHook(lpparam.classLoader))
                     startHook(QualityHook(lpparam.classLoader))
+                    startHook(VideoSubtitleHook(lpparam.classLoader))
                     startHook(PurifyShareHook(lpparam.classLoader))
                     startHook(DynamicHook(lpparam.classLoader))
                     startHook(ProtoBufHook(lpparam.classLoader))
