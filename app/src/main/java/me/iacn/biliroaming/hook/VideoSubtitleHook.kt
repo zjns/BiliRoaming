1ecc587de4111f869554d4f9d2dce8ef2a9b72f8
zjns <1638183271zjn@gmail.com>
2022-02-10 16:27:54 +0800

    Support translate chs to cht

--- app/src/main/java/me/iacn/biliroaming/hook/VideoSubtitleHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/VideoSubtitleHook.kt
@@ -6,33 +6,37 @@ import me.iacn.biliroaming.utils.*
 
 class VideoSubtitleHook(classLoader: ClassLoader) : BaseHook(classLoader) {
 
-    private val host = "https://www.kofua.top/bsub/t2s"
+    private val host = "https://www.kofua.top/bsub/%s"
 
     override fun startHook() {
-        if (!sPrefs.getBoolean("auto_generate_chs_subtitle", false)) return
+        if (!sPrefs.getBoolean("auto_generate_subtitle", false)) return
 
         BiliBiliPackage.instance.videoSubtitleClass?.hookAfterMethod("getSubtitlesList") ret@{ param ->
             val subtitles = param.result as? List<*> ?: listOf<Any>()
             val subtitleItemClass = BiliBiliPackage.instance.subtitleItemClass ?: return@ret
             val subTypeClass = BiliBiliPackage.instance.subtitleTypeClass ?: return@ret
-            val lanCodes = subtitles.map { s -> s?.callMethodOrNullAs<String>("getLan") }
-            if ("zh-CN" !in lanCodes && "zh-Hant" in lanCodes) {
-                val zhHant = subtitles.find { s ->
-                    s?.callMethod("getLan") == "zh-Hant"
-                } ?: return@ret
-                val subUrl = zhHant.callMethodOrNullAs<String>("getSubtitleUrl") ?: return@ret
-                val zhHansUrl = Uri.parse(host).buildUpon()
-                    .appendQueryParameter("sub_url", subUrl)
+            val lanCodes = subtitles.map { it?.callMethodOrNullAs<String>("getLan") }
+            val genCN = "zh-Hant" in lanCodes && "zh-CN" !in lanCodes
+            val genHant = "zh-CN" in lanCodes && "zh-Hant" !in lanCodes
+            val origin = if (genCN) "zh-Hant" else if (genHant) "zh-CN" else ""
+            val target = if (genCN) "zh-CN" else if (genHant) "zh-Hant" else ""
+            val converter = if (genCN) "t2cn" else if (genHant) "cn2t" else ""
+            val targetDoc = if (genCN) "简中（生成）" else if (genHant) "繁中（生成）" else ""
+            if (origin.isNotEmpty()) {
+                val origSub = subtitles.find { it?.callMethod("getLan") == origin } ?: return@ret
+                val origSubUrl = origSub.callMethodOrNullAs<String>("getSubtitleUrl") ?: return@ret
+                val targetSubUrl = Uri.parse(host.format(converter)).buildUpon()
+                    .appendQueryParameter("sub_url", origSubUrl)
                     .build().toString()
                 val ccType = subTypeClass.getStaticObjectFieldOrNull("CC") ?: return@ret
-                var id = zhHant.callMethodOrNullAs<Long>("getId") ?: 0L
+                var origSubId = origSub.callMethodOrNullAs<Long>("getId") ?: 0L
                 val item = subtitleItemClass.new().apply {
-                    callMethod("setLan", "zh-CN")
-                    callMethod("setLanDoc", "简中（生成）")
-                    callMethod("setSubtitleUrl", zhHansUrl)
+                    callMethod("setLan", target)
+                    callMethod("setLanDoc", targetDoc)
+                    callMethod("setSubtitleUrl", targetSubUrl)
                     callMethod("setType", ccType)
-                    callMethod("setId", ++id)
-                    callMethod("setIdStr", id.toString())
+                    callMethod("setId", ++origSubId)
+                    callMethod("setIdStr", origSubId.toString())
                 }
                 param.thisObject.callMethod("addSubtitles", subtitles.size, item)
                 param.result = param.thisObject.callMethod("getSubtitlesList")
