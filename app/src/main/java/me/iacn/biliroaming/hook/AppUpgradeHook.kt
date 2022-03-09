0e18f9231ed2c6bc596c627beb208d405891998c
zjns <1638183271zjn@gmail.com>
2022-03-09 16:10:24 +0800

    Fix upgrade install failed issue

--- app/src/main/java/me/iacn/biliroaming/hook/AppUpgradeHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/AppUpgradeHook.kt
@@ -2,17 +2,22 @@ package me.iacn.biliroaming.hook
 
 import me.iacn.biliroaming.BiliBiliPackage
 import me.iacn.biliroaming.BuildConfig
-import me.iacn.biliroaming.utils.findClass
-import me.iacn.biliroaming.utils.replaceMethod
+import me.iacn.biliroaming.utils.*
+import java.io.File
 
 class AppUpgradeHook(private val classLoader: ClassLoader) : BaseHook(classLoader) {
     private val upgradeApi = "https://www.kofua.top/bapp/version/upgrade/m%s"
 
     override fun startHook() {
-        val updaterOptionsClass = BiliBiliPackage.instance.updaterOptionsClass
-        val upgradeApiMethod = BiliBiliPackage.instance.upgradeApiMethod
-        updaterOptionsClass?.findClass(classLoader)?.replaceMethod(upgradeApiMethod) {
-            upgradeApi.format(BuildConfig.VERSION_CODE)
+        BiliBiliPackage.instance.updaterOptionsClass?.findClass(classLoader)?.run {
+            BiliBiliPackage.instance.upgradeApiMethod?.let {
+                replaceMethod(it) { upgradeApi.format(BuildConfig.VERSION_CODE) }
+            }
+        }
+        BiliBiliPackage.instance.appendChannelClass?.findClass(classLoader)?.run {
+            declaredFields.find { it.type == File::class.java }?.name?.let { field ->
+                replaceMethod("call") { it.thisObject.getObjectField(field) }
+            }
         }
     }
 }
