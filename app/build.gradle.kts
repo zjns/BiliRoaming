f6d88fdd153b0246f4ca54df81a6896a9436d2c8
zjns <1638183271zjn@gmail.com>
2022-06-04 18:41:41 +0800

    Check app updates locally

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -162,6 +162,7 @@ android {
             "-Xno-call-assertions",
             "-Xno-receiver-assertions",
             "-opt-in=kotlin.RequiresOptIn",
+            "-Xcontext-receivers"
         )
     }
 
