35b7a6d9abd42a8b7b8af3348ea2c09ae5410351
zjns <1638183271zjn@gmail.com>
2022-06-11 01:20:00 +0800

    Fix compile error

--- app/src/main/java/me/iacn/biliroaming/utils/Json.kt
+++ app/src/main/java/me/iacn/biliroaming/utils/Json.kt
@@ -2,7 +2,7 @@ package me.iacn.biliroaming.utils
 
 import org.json.JSONObject
 
-fun json(build: JSONObject.() -> Unit) = JSONObject().apply { build() }
+fun json(build: JSONObject.() -> Unit) = JSONObject().apply(build)
 
 context(JSONObject)
 infix fun String.by(build: JSONObject.() -> Unit): JSONObject = put(this, JSONObject().build())
