3c7d7c6508218272f0f01ff348e26658df166dd6
zjns <1638183271zjn@gmail.com>
2022-06-11 21:56:01 +0800

    Support prefer video tab in dynamic page

--- app/src/main/java/me/iacn/biliroaming/SettingDialog.kt
+++ app/src/main/java/me/iacn/biliroaming/SettingDialog.kt
@@ -71,7 +71,7 @@ class SettingDialog(context: Context) : AlertDialog.Builder(context) {
             findPreference("custom_splash")?.onPreferenceChangeListener = this
             findPreference("custom_splash_logo")?.onPreferenceChangeListener = this
             findPreference("save_log")?.summary =
-                context.resources.getString(R.string.save_log_summary).format(logFile.absolutePath)
+                context.getString(R.string.save_log_summary).format(logFile.absolutePath)
             findPreference("custom_server")?.onPreferenceClickListener = this
             findPreference("test_upos")?.onPreferenceClickListener = this
             findPreference("customize_bottom_bar")?.onPreferenceClickListener = this
@@ -99,7 +99,7 @@ class SettingDialog(context: Context) : AlertDialog.Builder(context) {
         private var mNewestTag = ""
 
         private fun checkUpdate() {
-            val url = URL(context.resources.getString(R.string.version_url))
+            val url = URL(context.getString(R.string.version_url))
             scope.launch {
                 val json = fetchJsonArray(url) ?: return@launch
                 for (result in json) {
@@ -165,7 +165,7 @@ class SettingDialog(context: Context) : AlertDialog.Builder(context) {
             if (!supportMusicNotificationHook) {
                 disablePreference(
                     "music_notification",
-                    context.resources.getString(R.string.os_not_support)
+                    context.getString(R.string.os_not_support)
                 )
             }
             if (!supportMain) {
@@ -198,7 +198,7 @@ class SettingDialog(context: Context) : AlertDialog.Builder(context) {
 
         private fun disablePreference(
             name: String,
-            message: String = context.resources.getString(R.string.not_support)
+            message: String = context.getString(R.string.not_support)
         ) {
             findPreference(name)?.run {
                 isEnabled = false
@@ -333,7 +333,7 @@ class SettingDialog(context: Context) : AlertDialog.Builder(context) {
         }
 
         private fun onUpdateClick(): Boolean {
-            val uri = Uri.parse(context.resources.getString(R.string.update_url, mNewestTag))
+            val uri = Uri.parse(context.getString(R.string.update_url, mNewestTag))
             val intent = Intent(Intent.ACTION_VIEW, uri)
             startActivity(intent)
             return true
@@ -366,7 +366,7 @@ class SettingDialog(context: Context) : AlertDialog.Builder(context) {
                     }
                 }
                 setNegativeButton("获取公共解析服务器") { _, _ ->
-                    val uri = Uri.parse(context.resources.getString(R.string.server_url))
+                    val uri = Uri.parse(context.getString(R.string.server_url))
                     val intent = Intent(Intent.ACTION_VIEW, uri)
                     startActivity(intent)
                 }
@@ -388,7 +388,7 @@ class SettingDialog(context: Context) : AlertDialog.Builder(context) {
                     if (it.isEmpty() || ids.contains(it)) return@forEach
                     bottomItems.add(JsonHook.BottomItem("未知", null, it, false))
                 }
-                setTitle(context.resources.getString(R.string.customize_bottom_bar_title))
+                setTitle(context.getString(R.string.customize_bottom_bar_title))
                 setPositiveButton(android.R.string.ok) { _, _ ->
                     val hideItems = mutableSetOf<String>()
                     bottomItems.forEach {
@@ -486,7 +486,7 @@ class SettingDialog(context: Context) : AlertDialog.Builder(context) {
                 return true
             }
             AlertDialog.Builder(activity)
-                .setTitle(context.resources.getString(R.string.share_log_title))
+                .setTitle(context.getString(R.string.share_log_title))
                 .setItems(arrayOf("log.txt", "old_log.txt (崩溃相关发这个)")) { _, witch ->
                     val toShareLog = when (witch) {
                         0 -> logFile
@@ -504,7 +504,7 @@ class SettingDialog(context: Context) : AlertDialog.Builder(context) {
                             putExtra(Intent.EXTRA_STREAM, uri)
                             addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
                             setDataAndType(uri, "text/log")
-                        }, context.resources.getString(R.string.share_log_title)))
+                        }, context.getString(R.string.share_log_title)))
                     } else {
                         Log.toast("日志文件不存在", force = true)
                     }
@@ -521,7 +521,7 @@ class SettingDialog(context: Context) : AlertDialog.Builder(context) {
                     if (it.isEmpty() || ids.contains(it)) return@forEach
                     JsonHook.drawerItems.add(JsonHook.BottomItem("未知", null, it, false))
                 }
-                setTitle(context.resources.getString(R.string.customize_drawer_title))
+                setTitle(context.getString(R.string.customize_drawer_title))
                 setPositiveButton(android.R.string.ok) { _, _ ->
                     val hideItems = mutableSetOf<String>()
                     JsonHook.drawerItems.forEach {
@@ -553,7 +553,7 @@ class SettingDialog(context: Context) : AlertDialog.Builder(context) {
             tv.setText(sPrefs.getString("custom_link", ""))
             tv.hint = "bilibili://user_center/vip"
             AlertDialog.Builder(activity).run {
-                setTitle(context.resources.getString(R.string.custom_link_summary))
+                setTitle(context.getString(R.string.custom_link_summary))
                 setView(tv)
                 setPositiveButton(android.R.string.ok) { _, _ ->
                     if (tv.text.toString().startsWith("bilibili://")) {
