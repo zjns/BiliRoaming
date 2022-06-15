f0545e6e6724726ee1d9b4e68f04bc21b9c65539
zjns <1638183271zjn@gmail.com>
2022-06-15 14:43:18 +0800

    Fix upgrade check error

--- app/src/main/java/me/iacn/biliroaming/utils/Json.kt
+++ app/src/main/java/me/iacn/biliroaming/utils/Json.kt
@@ -1,6 +1,9 @@
+@file:Suppress("NOTHING_TO_INLINE")
 package me.iacn.biliroaming.utils
 
 import org.json.JSONObject
+
+inline fun Map<String, Any>.toJson() = JSONObject(this).toString()
 
 fun json(build: JSONObject.() -> Unit) = JSONObject().apply(build)
 
