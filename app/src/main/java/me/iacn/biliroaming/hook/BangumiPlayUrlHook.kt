8726813c7c812a1634ab761f711c513954950f8e
zjns <1638183271zjn@gmail.com>
2022-08-01 20:29:07 +0800

    Add download subtitle feature

--- app/src/main/java/me/iacn/biliroaming/hook/BangumiPlayUrlHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/BangumiPlayUrlHook.kt
@@ -189,11 +189,15 @@ class BangumiPlayUrlHook(classLoader: ClassLoader) : BaseHook(classLoader) {
                         )
                         Log.toast("请求解析服务器发生错误: ${e.message}", alsoLog = true)
                     }
-                } else if (isDownload) {
-                    param.result = fixDownloadProto(response)
-                } else if (blockBangumiPageAds) {
-                    param.result = purifyViewInfo(response)
+                } else {
+                    lastSeasonInfo["epid"] = request.callMethod("getEpId")?.toString()
+                    if (isDownload) {
+                        param.result = fixDownloadProto(response)
+                    } else if (blockBangumiPageAds) {
+                        param.result = purifyViewInfo(response)
+                    }
                 }
+                VideoSubtitleHook.onEpPlay()
             }
         }
         "com.bapis.bilibili.pgc.gateway.player.v2.PlayURLMoss".findClassOrNull(mClassLoader)?.run {
@@ -265,11 +269,15 @@ class BangumiPlayUrlHook(classLoader: ClassLoader) : BaseHook(classLoader) {
                         )
                         Log.toast("请求解析服务器发生错误: ${e.message}", alsoLog = true)
                     }
-                } else if (isDownload) {
-                    param.result = fixDownloadProto(response)
-                } else if (blockBangumiPageAds) {
-                    param.result = purifyViewInfo(response)
+                } else {
+                    lastSeasonInfo["epid"] = request.callMethod("getEpId")?.toString()
+                    if (isDownload) {
+                        param.result = fixDownloadProto(response)
+                    } else if (blockBangumiPageAds) {
+                        param.result = purifyViewInfo(response)
+                    }
                 }
+                VideoSubtitleHook.onEpPlay()
             }
         }
         instance.playerMossClass?.run {
@@ -342,10 +350,14 @@ class BangumiPlayUrlHook(classLoader: ClassLoader) : BaseHook(classLoader) {
                         )
                         Log.toast("请求解析服务器发生错误: ${e.message}", alsoLog = true)
                     }
-                } else if (isDownload) {
-                    param.result = fixDownloadProtoUnite(response)
-                } else if (blockBangumiPageAds) {
-                    param.result = purifyViewInfo(response, supplement)
+                } else {
+                    lastSeasonInfo["epid"] = request.callMethod("getVod")
+                        ?.callMethodAs<Map<String, String>>("getExtraContentMap")?.get("ep_id")
+                    if (isDownload) {
+                        param.result = fixDownloadProtoUnite(response)
+                    } else if (blockBangumiPageAds) {
+                        param.result = purifyViewInfo(response, supplement)
+                    }
                 }
             }
         }
