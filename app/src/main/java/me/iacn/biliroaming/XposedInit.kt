653182d1f8911f1d7db073993428c93b434b6abe
zjns <1638183271zjn@gmail.com>
2022-08-25 09:17:00 +0800

    Separate subtitle download hook

--- app/src/main/java/me/iacn/biliroaming/XposedInit.kt
+++ app/src/main/java/me/iacn/biliroaming/XposedInit.kt
@@ -108,6 +108,7 @@ class XposedInit : IXposedHookLoadPackage, IXposedHookZygoteInit {
                     startHook(QualityHook(lpparam.classLoader))
                     startHook(VideoSubtitleHook(lpparam.classLoader))
                     startHook(DarkSwitchHook(lpparam.classLoader))
+                    startHook(SubtitleDownloadHook(lpparam.classLoader))
                     startHook(PurifyShareHook(lpparam.classLoader))
                     startHook(DynamicHook(lpparam.classLoader))
                     startHook(ProtoBufHook(lpparam.classLoader))
