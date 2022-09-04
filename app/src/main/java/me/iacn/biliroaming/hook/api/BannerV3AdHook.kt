1fbc66ca4f146c55f807496505cfa51a604f4333
zjns <1638183271zjn@gmail.com>
2022-09-04 15:30:28 +0800

    Support purify banner ads

---
+++ app/src/main/java/me/iacn/biliroaming/hook/api/BannerV3AdHook.kt
@@ -0,0 +1,48 @@
+package me.iacn.biliroaming.hook.api
+
+import me.iacn.biliroaming.utils.iterator
+import me.iacn.biliroaming.utils.sPrefs
+import org.json.JSONObject
+
+object BannerV3AdHook : ApiHook {
+    private val targetApis = arrayOf(
+        // 追番
+        "https://api.bilibili.com/pgc/page/bangumi",
+        // 影视
+        "https://api.bilibili.com/pgc/page/cinema/tab"
+    )
+
+    override val enabled: Boolean
+        get() = sPrefs.getBoolean("hidden", false)
+                && sPrefs.getBoolean("purify_banner_ads", false)
+
+    override fun canHandler(api: String) = targetApis.any { api.startsWith(it) }
+
+    override fun hook(response: String): String {
+        val json = JSONObject(response)
+        val modules = json.optJSONObject("result")
+            ?.optJSONArray("modules")
+            ?: return response
+        var changed = false
+        for (module in modules) {
+            if (module.optString("style") != "banner_v3")
+                continue
+            val items = module.optJSONArray("items")
+                ?: continue
+            val toRemoveIdx = mutableListOf<Int>()
+            var index = 0
+            for (item in items) {
+                if (item.optJSONObject("source_content")
+                        ?.optJSONObject("ad_content") != null
+                ) toRemoveIdx.add(index)
+                index++
+            }
+            if (toRemoveIdx.isNotEmpty())
+                changed = true
+            toRemoveIdx.reversed().forEach {
+                items.remove(it)
+            }
+        }
+        return if (changed) json.toString() else response
+    }
+}
