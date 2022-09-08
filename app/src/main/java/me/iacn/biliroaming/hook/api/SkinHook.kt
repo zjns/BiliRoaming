2f9e2375d4e6ce065e0dd407b0d4c4d83d66c72d
zjns <1638183271zjn@gmail.com>
2022-09-08 11:46:54 +0800

    Support import custom skin

---
+++ app/src/main/java/me/iacn/biliroaming/hook/api/SkinHook.kt
@@ -0,0 +1,31 @@
+package me.iacn.biliroaming.hook.api
+
+import me.iacn.biliroaming.utils.runCatchingOrNull
+import me.iacn.biliroaming.utils.sPrefs
+import me.iacn.biliroaming.utils.toJSONObject
+import org.json.JSONObject
+
+object SkinHook : ApiHook {
+    private const val skinApi = "https://app.bilibili.com/x/resource/show/skin"
+
+    override val enabled: Boolean
+        get() = sPrefs.getBoolean("hidden", false)
+                && sPrefs.getBoolean("skin", false)
+                && !sPrefs.getString("skin_json", null).isNullOrEmpty()
+
+    override fun canHandler(api: String) = api.startsWith(skinApi)
+
+    override fun hook(response: String): String {
+        val skinJson = sPrefs.getString("skin_json", null)
+        val skin = skinJson.runCatchingOrNull { toJSONObject() }
+            ?.apply {
+                if (optString("package_md5").isNotEmpty())
+                    put("package_md5", JSONObject.NULL)
+            } ?: return response
+        return response.toJSONObject()
+            .apply {
+                optJSONObject("data")
+                    ?.put("user_equip", skin)
+            }.toString()
+    }
+}
