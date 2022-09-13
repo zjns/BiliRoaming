2de0af6edddc21c084277211a0b709f087b3cd0c
zjns <1638183271zjn@gmail.com>
2022-09-14 00:02:21 +0800

    Support custom text folding

--- app/src/main/java/me/iacn/biliroaming/XposedInit.kt
+++ app/src/main/java/me/iacn/biliroaming/XposedInit.kt
@@ -111,6 +111,7 @@ class XposedInit : IXposedHookLoadPackage, IXposedHookZygoteInit {
                     startHook(DarkSwitchHook(lpparam.classLoader))
                     startHook(SubtitleDownloadHook(lpparam.classLoader))
                     startHook(OkHttpHook(lpparam.classLoader))
+                    startHook(TextFoldHook(lpparam.classLoader))
                     startHook(PurifyShareHook(lpparam.classLoader))
                     startHook(DynamicHook(lpparam.classLoader))
                     startHook(ProtoBufHook(lpparam.classLoader))
