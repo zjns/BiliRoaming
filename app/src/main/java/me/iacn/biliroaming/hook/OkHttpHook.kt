0ddb4b776603bc1b5b6fa4ffb443954789d97030
zjns <1638183271zjn@gmail.com>
2022-10-01 16:40:28 +0800

    Better OkHttp Hook

--- app/src/main/java/me/iacn/biliroaming/hook/OkHttpHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/OkHttpHook.kt
@@ -20,40 +20,37 @@ class OkHttpHook(classLoader: ClassLoader) : BaseHook(classLoader) {
     override fun startHook() {
         if (apiHooks.all { !it.enabled }) return
 
-        instance.realCallClass?.hookAfterMethod(instance.execute()) { param ->
-            val response = param.result ?: return@hookAfterMethod
-            val requestField = instance.realCallRequestField() ?: return@hookAfterMethod
-            val urlField = instance.urlField() ?: return@hookAfterMethod
-            val request = param.thisObject.getObjectField(requestField) ?: return@hookAfterMethod
-            val url = request.getObjectField(urlField)?.toString() ?: return@hookAfterMethod
+        instance.responseClass?.hookAfterAllConstructors out@{ param ->
+            val response = param.thisObject ?: return@out
+            val requestField = instance.requestField() ?: return@out
+            val urlField = instance.urlField() ?: return@out
+            val request = response.getObjectField(requestField) ?: return@out
+            val url = request.getObjectField(urlField)?.toString() ?: return@out
             for (hook in apiHooks) {
                 if (!hook.enabled || !hook.canHandler(url))
                     continue
-                val okioClass = instance.okioClass ?: return@hookAfterMethod
-                val bufferedSourceClass = instance.bufferedSourceClass ?: return@hookAfterMethod
-                val codeField = instance.codeField() ?: return@hookAfterMethod
-                val bodyField = instance.bodyField() ?: return@hookAfterMethod
-                val stringMethod = instance.string() ?: return@hookAfterMethod
-                val sourceMethod = instance.source() ?: return@hookAfterMethod
-                val bufferMethod = instance.sourceBuffer() ?: return@hookAfterMethod
+                val okioClass = instance.okioClass ?: return@out
+                val bufferedSourceClass = instance.bufferedSourceClass ?: return@out
+                val codeField = instance.codeField() ?: return@out
+                val bodyField = instance.bodyField() ?: return@out
+                val stringMethod = instance.string() ?: return@out
+                val sourceMethod = instance.source() ?: return@out
+                val bufferMethod = instance.sourceBuffer() ?: return@out
                 response.getIntField(codeField).takeIf { it == HttpURLConnection.HTTP_OK }
-                    ?: return@hookAfterMethod
+                    ?: return@out
 
                 val responseBody = response.getObjectField(bodyField)
-                val respString = responseBody?.callMethod(stringMethod)
-                    ?.toString() ?: return@hookAfterMethod
-                val sourceField = responseBody.javaClass.findFieldByExactType(bufferedSourceClass)
-                    ?: return@hookAfterMethod
+                val sourceField = responseBody?.javaClass
+                    ?.findFieldByExactTypeOrNull(bufferedSourceClass) ?: return@out
                 val longType = Long::class.javaPrimitiveType!!
-                val contentLengthField = responseBody.javaClass.findFieldByExactType(longType)
-                    ?: return@hookAfterMethod
+                val contentLengthField = responseBody.javaClass.findFieldByExactTypeOrNull(longType)
+                    ?: return@out
+                val respString = responseBody.callMethod(stringMethod)?.toString() ?: return@out
                 val newResponse = hook.hook(respString)
                 val stream = newResponse.byteInputStream()
                 val length = stream.available()
-                val source = okioClass.callStaticMethod(sourceMethod, stream)
-                    ?: return@hookAfterMethod
-                val bufferedSource = okioClass.callStaticMethod(bufferMethod, source)
-                    ?: return@hookAfterMethod
+                val source = okioClass.callStaticMethod(sourceMethod, stream) ?: return@out
+                val bufferedSource = okioClass.callStaticMethod(bufferMethod, source) ?: return@out
                 sourceField.set(responseBody, bufferedSource)
                 contentLengthField.set(responseBody, length)
                 break
