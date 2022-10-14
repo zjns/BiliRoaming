ba5f1682e63f110df6a95fdd7cfd4b4a8fa362d6
Kofua <1638183271zjn@gmail.com>
2022-10-14 11:25:32 +0800

    Add some purify features

---
+++ app/src/main/java/me/iacn/biliroaming/MiscRemoveAdsDialog.kt
@@ -0,0 +1,110 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
+package me.iacn.biliroaming
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.SharedPreferences
+import android.graphics.Color
+import android.text.TextUtils
+import android.util.TypedValue
+import android.view.Gravity
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.Switch
+import android.widget.TextView
+import me.iacn.biliroaming.utils.Log
+import me.iacn.biliroaming.utils.addBackgroundRipple
+import kotlin.math.roundToInt
+
+class MiscRemoveAdsDialog(val activity: Activity, prefs: SharedPreferences) :
+    AlertDialog.Builder(activity) {
+    init {
+        val scrollView = ScrollView(context).apply {
+            scrollBarStyle = ScrollView.SCROLLBARS_OUTSIDE_OVERLAY
+        }
+        val root = LinearLayout(context).apply {
+            orientation = LinearLayout.VERTICAL
+            layoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT
+            )
+        }
+        scrollView.addView(root)
+
+        val removeSearchAdsSwitch = string(R.string.remove_search_ads_title).let {
+            switchPrefsItem(it).let { p -> root.addView(p.first); p.second }
+        }
+        removeSearchAdsSwitch.isChecked = prefs.getBoolean("remove_search_ads", false)
+
+        val removeCommentCmSwitch = string(R.string.remove_comment_cm_title).let {
+            switchPrefsItem(it).let { p -> root.addView(p.first); p.second }
+        }
+        removeCommentCmSwitch.isChecked = prefs.getBoolean("remove_comment_cm", false)
+
+        setTitle(string(R.string.misc_remove_ads_title))
+
+        setPositiveButton(android.R.string.ok) { _, _ ->
+            prefs.edit().apply {
+                putBoolean("remove_search_ads", removeSearchAdsSwitch.isChecked)
+                putBoolean("remove_comment_cm", removeCommentCmSwitch.isChecked)
+            }.apply()
+            Log.toast(string(R.string.prefs_save_success_and_reboot))
+        }
+        setNegativeButton(android.R.string.cancel, null)
+
+        root.setPadding(16.dp, 10.dp, 16.dp, 10.dp)
+
+        setView(scrollView)
+    }
+
+    private val Int.dp
+        inline get() = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            toFloat(),
+            context.resources.displayMetrics
+        ).roundToInt()
+
+    private inline fun string(resId: Int) = context.getString(resId)
+
+    private fun switchPrefsItem(title: String): Pair<LinearLayout, Switch> {
+        val layout = LinearLayout(context).apply {
+            gravity = Gravity.CENTER_VERTICAL
+            orientation = LinearLayout.HORIZONTAL
+            layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+        }
+        val titleView = TextView(context).apply {
+            text = title
+            setTextSize(TypedValue.COMPLEX_UNIT_SP, 16F)
+            setSingleLine()
+            ellipsize = TextUtils.TruncateAt.END
+            setPadding(0, 8.dp, 0, 8.dp)
+            layoutParams = LinearLayout.LayoutParams(
+                0,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            ).apply {
+                weight = 1F
+                marginEnd = 10.dp
+            }
+        }
+        val switcher = Switch(context).apply {
+            isClickable = false
+            isSoundEffectsEnabled = false
+            isHapticFeedbackEnabled = false
+            setBackgroundColor(Color.TRANSPARENT)
+            layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+        }
+        layout.setOnClickListener { switcher.toggle() }
+        layout.addBackgroundRipple()
+        layout.addView(titleView)
+        layout.addView(switcher)
+        return Pair(layout, switcher)
+    }
+}
