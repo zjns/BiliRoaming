2f914cb748e71c61b1058255826f38cbf75bfbd9
Kofua <1638183271zjn@gmail.com>
2022-10-14 11:25:32 +0800

    Add some purify features

--- app/src/main/java/me/iacn/biliroaming/SettingDialog.kt
+++ app/src/main/java/me/iacn/biliroaming/SettingDialog.kt
@@ -93,6 +93,7 @@ class SettingDialog(context: Context) : AlertDialog.Builder(context) {
             findPreference("add_custom_button")?.onPreferenceChangeListener = this
             findPreference("skin")?.onPreferenceChangeListener = this
             findPreference("text_fold")?.onPreferenceClickListener = this
+            findPreference("misc_remove_ads")?.onPreferenceClickListener = this
             findPreference("customize_dynamic")?.onPreferenceClickListener = this
             checkCompatibleVersion()
             checkUpdate()
@@ -733,6 +734,7 @@ class SettingDialog(context: Context) : AlertDialog.Builder(context) {
             "customize_drawer" -> onCustomizeDrawerClick()
             "custom_link" -> onCustomLinkClick()
             "text_fold" -> onTextFoldClick()
+            "misc_remove_ads" -> run { MiscRemoveAdsDialog(activity, prefs).show(); true }
             "customize_dynamic" -> onCustomDynamicClick()
             else -> false
         }
