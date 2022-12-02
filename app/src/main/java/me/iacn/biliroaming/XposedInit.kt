7dd95b7083e9b90aea5816c3bef0d3cc7e614c3e
Kofua <1638183271zjn@gmail.com>
2022-12-02 13:59:26 +0800

    Fix crash on play version

--- app/src/main/java/me/iacn/biliroaming/XposedInit.kt
+++ app/src/main/java/me/iacn/biliroaming/XposedInit.kt
@@ -107,7 +107,7 @@ class XposedInit : IXposedHookLoadPackage, IXposedHookZygoteInit {
                     startHook(CopyHook(lpparam.classLoader))
                     startHook(LiveRoomHook(lpparam.classLoader))
                     startHook(QualityHook(lpparam.classLoader))
-                    if (platform != "android_hd")
+                    if (platform == "android")
                         startHook(DarkSwitchHook(lpparam.classLoader))
                     startHook(SubtitleDownloadHook(lpparam.classLoader))
                     startHook(OkHttpHook(lpparam.classLoader))
