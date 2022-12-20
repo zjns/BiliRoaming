06d3bb8787f59845b5a703d747bc7b8201308b81
Kofua <1638183271zjn@gmail.com>
2022-12-20 11:51:43 +0800

    Fix download subtitle

--- app/src/main/java/me/iacn/biliroaming/utils/Utils.kt
+++ app/src/main/java/me/iacn/biliroaming/utils/Utils.kt
@@ -114,9 +114,6 @@ val platform by lazy {
         }
 }
 
-val Any?.isNull get() = this == null
-val Any?.notNull get() = this != null
-
 val logFile by lazy { File(currentContext.externalCacheDir, "log.txt") }
 
 val oldLogFile by lazy { File(currentContext.externalCacheDir, "old_log.txt") }
@@ -302,6 +299,7 @@ fun View.addBackgroundRipple() = with(TypedValue()) {
     setBackgroundResource(resourceId)
 }
 
+@Suppress("DEPRECATION")
 fun windowAlertPermissionGranted(): Boolean {
     val context = currentContext
     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
