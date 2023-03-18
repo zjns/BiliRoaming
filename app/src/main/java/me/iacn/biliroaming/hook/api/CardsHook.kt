5bc1adc7a4f46e0fac0787f876d1c6fc8f4a2976
Kofua <1638183271zjn@gmail.com>
2023-03-18 23:34:17 +0800

    Update api hooks

--- app/src/main/java/me/iacn/biliroaming/hook/api/CardsHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/api/CardsHook.kt
@@ -7,9 +7,10 @@ import org.json.JSONObject
 object CardsHook : ApiHook {
     private const val cardsApi = "https://api.bilibili.com/pgc/season/player/cards"
 
-    override val enabled: Boolean
-        get() = sPrefs.getBoolean("hidden", false)
+    override val enabled by lazy {
+        sPrefs.getBoolean("hidden", false)
                 && sPrefs.getBoolean("block_up_rcmd_ads", false)
+    }
 
     override fun canHandler(api: String) = api.startsWith(cardsApi)
 
