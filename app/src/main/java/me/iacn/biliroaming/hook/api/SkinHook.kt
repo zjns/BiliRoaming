bd9bd8f9846ef1ae2ff0b7bcf6bd7536ff43d3ba
Kofua <1638183271zjn@gmail.com>
2023-03-18 23:34:17 +0800

    Update api hooks

--- app/src/main/java/me/iacn/biliroaming/hook/api/SkinHook.kt
+++ app/src/main/java/me/iacn/biliroaming/hook/api/SkinHook.kt
@@ -8,10 +8,11 @@ import org.json.JSONObject
 object SkinHook : ApiHook {
     private const val skinApi = "https://app.bilibili.com/x/resource/show/skin"
 
-    override val enabled: Boolean
-        get() = sPrefs.getBoolean("hidden", false)
+    override val enabled by lazy {
+        sPrefs.getBoolean("hidden", false)
                 && sPrefs.getBoolean("skin", false)
                 && !sPrefs.getString("skin_json", null).isNullOrEmpty()
+    }
 
     override fun canHandler(api: String) = api.startsWith(skinApi)
 
